fd333e7417ab67ba4f823b9bf552ea9db3f9d9db
David <david.recuencogadea@gmail.com>
2016-02-04 22:07:28 +0100

    INITIAL COMMIT

---
+++ README.md
@@ -0,0 +1,21 @@
+# picbot-tumblr-plugin
+
+A source plugin for twitter-picbot which retrieves sources from tumblr.
+
+## Credentials
+
+You will need to register an account and
+an application in it, once you have both, you will get a **consumer_key**,
+place it in the root folder of the plugin into **keys.json** file like:
+
+```
+{
+  "consumer_key": "consumer-key"
+}
+```
+
+## Requirements
+
+Make sure the following npm packages are included in your **twitter-picbot** application.
+
+- tumblr.js
---
+++ index.js
@@ -0,0 +1,45 @@
+'use strict';
+
+const tumblr = require('tumblr.js'),
+  utils = {
+    /**
+     * Gets a random item from given array.
+     * @param {Array} list - an array.
+     * @returns {*} random item fron the array.
+     */
+    getRandom(list) {
+      return list[Math.floor(Math.random() * (list.length - 1))];
+    }
+  };
+
+module.exports = {
+  /**
+   * Gets source of an image related to the query.
+   * @param {string} query - topic of the image.
+   * @return {Promise} promise handler.
+   */
+  getSource: function(query) {
+    return new Promise((resolve, reject) => {
+      const KEYS = require('./keys.json'),
+        client = tumblr.createClient(KEYS);
+
+      client.tagged(query, {limit: 50}, (error, data) => {
+        if (error) {
+          reject({
+            error
+          });
+        } else {
+          let imagePosts = data.filter((post) => {
+              return post.type === 'photo'
+            }),
+            source = utils.getRandom(imagePosts);
+
+          resolve({
+            url: source.short_url,
+            source: source.photos[0].original_size.url
+          });
+        }
+      });
+    });
+  }
+};
